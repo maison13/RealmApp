57e347750f8bd87c9f0f8f8a641a22ff8850ace2
Vladimir <smile_friend@mail.ru>
2023-05-24 18:52:51 +0300

    Initial Commit

---
+++ RealmApp/Services/StorageManager.swift
@@ -0,0 +1,94 @@
+//
+//  StorageManager.swift
+//  RealmApp
+//
+//  Created by Alexey Efimov on 08.10.2021.
+//  Copyright © 2021 Alexey Efimov. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+final class StorageManager {
+    static let shared = StorageManager()
+    
+    let realm: Realm
+    
+    private init() {
+        do {
+            realm = try Realm()
+        } catch {
+            fatalError("Failed to initialize Realm: \(error)")
+        }
+    }
+    
+    // MARK: - Task List
+    func save(_ taskLists: [TaskList]) {
+        write {
+            realm.add(taskLists)
+        }
+    }
+    
+    func save(_ taskList: String, completion: (TaskList) -> Void) {
+        write {
+            let taskList = TaskList(value: [taskList])
+            realm.add(taskList)
+            completion(taskList)
+        }
+    }
+    
+    func delete(_ taskList: TaskList) {
+        write {
+            realm.delete(taskList.tasks)
+            realm.delete(taskList)
+        }
+    }
+    
+    func edit(_ taskList: TaskList, newValue: String) {
+        write {
+            taskList.title = newValue
+        }
+    }
+
+    func done(_ taskList: TaskList) {
+        write {
+            taskList.tasks.setValue(true, forKey: "isComplete")
+        }
+    }
+
+    // MARK: - Tasks
+    func save(_ task: String, withNote note: String, to taskList: TaskList, completion: (Task) -> Void) {
+        write {
+            let task = Task(value: [task, note])
+            taskList.tasks.append(task)
+            completion(task)
+        }
+    }
+    func delete(tasks taskList: TaskList, for index: Int) {
+        write {
+            realm.delete(taskList.tasks[index])
+
+        }
+    }
+    
+    func edit(tasks taskList: TaskList, newValue: String) {
+        write {
+            taskList.title = newValue
+        }
+    }
+
+    func done(tasks taskList: TaskList) {
+        write {
+            taskList.tasks.setValue(true, forKey: "isComplete")
+        }
+    }
+    private func write(completion: () -> Void) {
+        do {
+            try realm.write {
+                completion()
+            }
+        } catch {
+            print(error)
+        }
+    }
+}
